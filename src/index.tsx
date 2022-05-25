b02e5a4a8777f5e9a95bb08a4d8412e4c0d4668b
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-05-25 16:27:24 -0300

    feat: update

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
+import { Text } from 'react-native';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a / b);
+}
+
+export function TextLabel(): Element {
+  return <Text>MUITO BEM MUITO BOM!</Text>;
 }
